b2de16c4fc29a30baa1cd8458177e8901df9253e
Andreas Buchholz <andy@talentsconnect.com>
2018-02-12 09:24:05 +0100

    added Makefile & fixed tests
    Signed-off-by: Andreas Buchholz <andy@talentsconnect.com>

--- Braumeister/tests/settings_test.py
+++ Braumeister/tests/settings_test.py
@@ -1,16 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 from unittest import TestCase, main
 from ..settings import Settings
+
 
 class SettingsTest(TestCase):
 
     def test_test(self):
         self.assertEqual("hund", "hund")
 
-    def test_has_config_file(self):
-        f = Settings.find_config_file()
-        print("file: %s" % f)
+    def test_create_config_file(self):
+        Settings.load()
+        config_file = Settings.find_config_file()
+
+        self.assertIsNotNone(config_file)
+
+    def test_set_string(self):
+        Settings.load()
+        Settings.save("general", "hund", "wuff")
+        val = Settings.get("general", "hund")
         
-        self.assertIsNotNone(f)
+        self.assertEqual(val, "wuff")
+    
+    def test_get_bool(self):
+        Settings.load()
+        verbose = Settings.get("general", "verbose")
+
+        self.assertEqual(verbose, False)
+
 
 if __name__ == '__main__':
     main()
---
+++ Makefile
@@ -0,0 +1,19 @@
+.DEFAULT_GOAL = usage
+
+.PHONY: clean
+
+clean:
+	-rm -rf build dist MANIFEST Braumeister.egg-info .braumeister
+	-find . -name '*.py[oc]' -exec rm {} \;
+
+test:
+	@make clean
+	python3 -m unittest discover -p *_test.py
+	@make clean
+
+usage:
+	@echo ""
+	@echo "make"
+	@echo "  clean   Cleans up the dirt"
+	@echo "  test    Execute all tests"
+	@echo ""
